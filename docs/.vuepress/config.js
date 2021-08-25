86079e18bfcff2f186ec88c4878284c508185a9e
win <wocwin@163.com>
2021-08-25 15:26:19 +0800

    polish:vuepress支持ts/tsx文件可以解析

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -94,7 +94,45 @@ module.exports = {
             ]
         }
     },
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'cache-loader',
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                babelrc: false,
+                                configFile: false,
+                                presets: [
+                                    '@babel/preset-env', // 可以识别es6语法
+                                    '@vue/babel-preset-jsx' // 解析jsx语法
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                appendTsxSuffixTo: [/\.vue$/, /\.md$/]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    },
     plugins: [
+        [
+            'vuepress-plugin-typescript',
+            {
+                tsLoaderOptions: {
+                    // ts-loader 的所有配置项
+                },
+            },
+        ],
         // ['@vuepress/plugins-back-to-top', false],
         ['vuepress-plugin-gotop-plus', {
             // mobileShow: false,

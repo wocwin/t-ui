59d3c4d1f8e22c7f5a11180e735a7578193c2a36
wocwin <wocwin@163.com>
2023-09-13 19:38:59 +0800

    ci:使用webpack打包生成index.js文件

---
+++ build/webpack.conf.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: ['./packages/index.js']
+  },
+  output: {
+    path: path.resolve(process.cwd(), './lib'),
+    publicPath: '/dist/',
+    filename: 'index.js',
+    chunkFilename: '[id].js',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    library: 'ELEMENT',
+    umdNamedDefine: true,
+    globalObject: 'typeof self !== \'undefined\' ? self : this'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      packages: path.resolve(__dirname, './packages'),
+      examples: path.resolve(__dirname, './examples')
+    }
+  },
+  externals: {
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  stats: {
+    children: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|babel|es6)$/,
+        include: process.cwd(),
+        exclude: /node_modules|utils\/popper\.js|utils\/date\.js/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
--- packages/index.js
+++ packages/index.js
@@ -146,6 +146,7 @@ export {
 }
 export default {
   version,
+  ...components,
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install
 }
